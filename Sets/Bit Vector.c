712b6bc55e9ae89cb9f22a5017b22fff491a4a42
23000003 <kurtdicdican@gmail.com>
2024-09-16 11:14:14 +0800

    Bit Vector Done

--- Sets/Bit Vector.c
+++ Sets/Bit Vector.c
@@ -19,31 +19,63 @@ bool isElement(Set s, int item);
 
 int main(){
     
-    Set a;
+    Set a, b;
+    
+    printf("===== Set a =====:\n");
     
     if (addElement(&a, 5)){
     	printf("%d\n", a);
 	}
     
-	if(addElement(&a, 5)){
-    	printf("%d\n", a);
-	}
+//	if(addElement(&a, 5)){
+//    	printf("%d\n", a);
+//	}
     
     if(addElement(&a, 1)){
     	printf("%d\n", a);
 	}
 	
-	displaySet(a);
-	
-	if(removeElement(&a, 2)){
-    	printf("%d\n", a);
-	}
-	
-	if(removeElement(&a, 1)){
-    	printf("%d\n", a);
-	}
+//	if(removeElement(&a, 2)){
+//    	printf("%d\n", a);
+//	}
+//	
+//	if(removeElement(&a, 1)){
+//    	printf("%d\n", a);
+//	}
 	
 	displaySet(a);
+	
+	printf("===== Set b =====:\n");
+	if(addElement(&b, 5)){
+    	printf("%d\n", b);
+	}
+	
+	if(addElement(&b, 7)){
+    	printf("%d\n", b);
+	}
+	
+	displaySet(b);
+	
+	printf("==== Union Set ====\n");
+	displaySet(unionSet(a, b));
+	
+	printf("==== Intersection Set ====\n");
+	displaySet(intersectionSet(a, b));
+	
+	printf("==== Difference Set of A ====\n");
+	displaySet(differenceSet(a, b));
+	
+	printf("==== Difference Set of B ====\n");
+	displaySet(differenceSet(b, a));
+	
+	printf("==== SymmetricDiff Set ====\n");
+	displaySet(symmetricDifferenceSet(a, b));
+	
+	printf("==== Cardinality of A ====\n");
+	printf("%d\n\n", cardinality(a));
+	
+	printf("==== isElement? ====\n");
+	(isElement(a,  5)) ? printf("Is Element\n") : printf("Is not an Element");
     
     return 0;
 }
@@ -51,6 +83,9 @@ int main(){
 // 0000 0000 0000 0000
 // 15 - 0 => value 
 bool addElement(Set *s, int item){
+	
+	if(item > 16 || item < 0) return false;
+	
 	Set temp = 1 << item;
 
 	//can be (*s >> item) == 1
@@ -65,6 +100,9 @@ bool addElement(Set *s, int item){
 }
 
 bool removeElement(Set *s, int item){
+	
+	if(item > 16 || item < 0) return false;
+	
 	Set temp = 1 << item;
 	
 	if((*s & temp) == 0){
@@ -82,63 +120,49 @@ void displaySet(Set s){
             printf("%d ",i);
         }
     }
-    printf("\n");
+    
+    // PRINTS IN BINARY FORMAT
+//    for (i = sizeof(s) * 8 - 1; i >= 0; i--) {
+//        printf("%d", (s >> i) & 1);
+//        if (i % 4 == 0) printf(" ");
+//    }
+    printf("\n\n");
 }
 
 Set unionSet(Set a, Set b){
-	
+	return a | b;
 }
 
 Set intersectionSet(Set a, Set b){
-
+	return a & b;
 }
-
+//ex.
+//a 0110 0101
+//b 0100 0110
+// if return difference of a = 0000 0010   if b = 0010 0001
 Set differenceSet(Set a, Set b){
-	
+	return a & ~b; // inverts "b" to check using "AND" on "a" if its present or not;
 }
 
 Set symmetricDifferenceSet(Set a, Set b){
-	
+	return a ^ b;
 }
 
 int cardinality(Set a){
-	
+	int i, count = 0;
+	for (i = 0; a > 0; i++, a = a >> 1){
+        if (a & 1){ 
+            count++;
+        }
+    }
+    
+    return count;
 }
 
 bool isElement(Set s, int item){
 	
+	return (s & (1 << item)) != 0;
 }
-
-//#include <stdio.h>      // USE BITWISE OPERATOR
-//int main()
-//{
-//    // a = 5 (00000101 in 8-bit binary), b = 9 (00001001 in
-//    // 8-bit binary)
-//    unsigned int a = 5, b = 9;
-//
-//    // The result is 00000001
-//    printf("a = %u, b = %u\n", a, b);
-//    printf("a&b = %u\n", a & b);
-//
-//    // The result is 00001101
-//    printf("a|b = %u\n", a | b);
-//
-//    // The result is 00001100
-//    printf("a^b = %u\n", a ^ b);
-//
-//    // The result is 11111111111111111111111111111010
-//    // (assuming 32-bit unsigned int)
-//    printf("~a = %u\n", a = ~a);
-//
-//    // The result is 00010010
-//    printf("b<<1 = %u\n", b << 1);
-//
-//    // The result is 00000100
-//    printf("b>>1 = %u\n", b >> 1);
-//
-//    return 0;
-//}
-//
 
 
 
